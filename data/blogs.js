f700ca01eecc9ea5e32255f5c2fe32c09d36a41e
Devendra Shukla <devshukla0123@gmail.com>
2024-06-01 14:17:58 +0530

    create a blog route and also data files to show data on access url

---
+++ data/blogs.js
@@ -0,0 +1,19 @@
+blogs = [
+    {
+        title: 'my first blog title',
+        content: 'this is my blog content',
+        slug: 'first-b'
+    },
+    {
+        title: 'my second blog title',
+        content: 'this is my blog content',
+        slug: 'second-b'
+    },
+    {
+        title: 'my third blog title',
+        content: 'this is my blog content',
+        slug: 'third-b'
+    }
+]
+
+module.exports = blogs;
--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const path = require("path");
 const router = express.Router(); //router
-
+const blogsdata = require("../data/blogs")
 
 router.get('/', (req, res) => {
-    console.log(__dirname,'../templates/index.html');
-    res.sendFile(path.join(__dirname,'../templates/index.html'));
+    res.sendFile(path.join(__dirname, '../templates/index.html'));
+})
+
+//show all blogs routes
+router.get('/blogs', (req, res) => {
+    blogsdata.forEach(element => {
+        console.log(element.title);
+    });
+    res.sendFile(path.join(__dirname, '../templates/blogHome.html'));
+})
+
+//show single blog
+router.get('/blogpost/:slug', (req, res) => {
+    myBlog = blogsdata.filter((e) => {
+        return e.slug == req.params.slug
+    });
+    console.log(myBlog,'myBlog');
+    res.sendFile(path.join(__dirname, '../templates/blogPage.html'));
 })
 
 module.exports = router; //make export show it will be used in index.js
