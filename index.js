1718931d96e42e86060d0b9c86dc908a5122452d
Devendra Shukla <devshukla0123@gmail.com>
2024-06-03 12:19:08 +0530

    commit changes

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const exphbs = require('express-handlebars');
 const path = require("path");
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname,"static")))
-app.use('/',require(path.join(__dirname,'routes/blog.js')))
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+//app.use(express.static(path.join(__dirname,"static")))
+//app.use('/',require(path.join(__dirname,'routes/blog.js')))
 
 
 app.listen(port, () => {
